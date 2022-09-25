e21c308ee5bbdf1dfabab65277c26660b2fb05aa
h3n4l <oysterdays@gmail.com>
2022-09-25 21:42:52 +0800

    refactor: move main.go in the root path

--- cmd/root.go
+++ cmd/root.go
@@ -5,7 +5,6 @@ package cmd
 
 import (
 	"fmt"
-	"os"
 
 	"github.com/spf13/cobra"
 )
@@ -38,16 +37,15 @@ var rootCmd = &cobra.Command{
 	Use:   "apd [command]",
 	Short: "Apple Data CLI",
 	Long:  `Apple Data CLI is a command line tool to process Apple part data.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		printGreeting()
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
-func Execute() {
-	err := rootCmd.Execute()
-	if err != nil {
-		os.Exit(1)
-	}
-	printGreeting()
+func Execute() error {
+	return rootCmd.Execute()
 }
 
 func init() {
